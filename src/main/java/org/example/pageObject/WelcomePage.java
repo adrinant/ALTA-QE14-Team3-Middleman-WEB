5881982b82dbf14872da5e87f58e7648aeb11ccb
dareldiarma <alvindarel@gmail.com>
2024-05-09 18:56:06 +0700

    Add register page xpath

--- src/main/java/org/example/pageObject/WelcomePage.java
+++ src/main/java/org/example/pageObject/WelcomePage.java
@@ -22,4 +22,23 @@ public class WelcomePage {
     private WebElement signInButton;
     @FindBy(xpath = "//button[@id='to-register']")
     private WebElement signUpButton;
+
+    //RegisterPage
+    @FindBy(xpath = "//h1[@class='text-black font-Roboto font-extrabold text-5xl md:text-6xl']")
+    private WebElement h1Hi;
+    @FindBy(xpath = "//h4[@class='text-black/50 font-Roboto text-2xl italic md:text-3xl']")
+    private WebElement h4Create;
+    @FindBy(xpath = "//input[@id='input-name']")
+    private WebElement shopNameField;
+    @FindBy(xpath = "//input[@id='input-email']")
+    private WebElement emailField;
+    @FindBy(xpath = "//input[@id='input-phone']")
+    private WebElement phoneNumberField;
+    @FindBy(xpath = "//input[@id='input-password']")
+    private WebElement passwordField;
+    @FindBy(xpath = "//input[@id='input-address']")
+    private WebElement addressField;
+    @FindBy(xpath = "//a[@id='to-login']")
+    private WebElement registerLoginButton;
+
 }
